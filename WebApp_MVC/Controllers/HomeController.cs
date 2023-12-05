2df68510c2add3b8503cad15981328f831466437
thithordev <128371722+thithordev@users.noreply.github.com>
2023-12-05 22:43:03 +0700

    buoi2

--- WebApp_MVC/Controllers/HomeController.cs
+++ WebApp_MVC/Controllers/HomeController.cs
@@ -13,9 +13,33 @@ namespace WebApp_MVC.Controllers
             _logger = logger;
         }
 
+        public IActionResult Test() {
+
+            ViewData["Message"] = "Tutor NET104";
+            ViewBag.ABC = "abc";
+            int namnay = 2023;
+            SanPham sp = new SanPham()
+            {
+                ID = Guid.NewGuid(),
+                TenSanPham = "Dien thoai 15 pro max",
+                Gia = 240000000,
+                Soluong = 100,
+                TrangThai = 1,
+            };
+
+            ViewData["SanPham"] = sp;
+            return View(namnay);
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult tangsodem() 
+        {
+        
+            return View("Test");
         }
 
         public IActionResult Privacy()
